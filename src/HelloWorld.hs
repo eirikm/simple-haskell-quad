c5970febd3dd25d1a842dc6caa00f52de147548b
Eirik Meland <eirik.meland@gmail.com>
2021-09-25 09:25:07 +0000

    add RIO as dep

--- src/HelloWorld.hs
+++
@@ -1,9 +0,0 @@
-module HelloWorld where
-
--- https://www.hackerrank.com/challenges/fp-hello-world/problem
-
-main :: IO ()
-main = interact helloWorld
-
-helloWorld :: String -> String
-helloWorld = const "Hello World"
