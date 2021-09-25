f5222531ea4e3ec007f7b4ecbe733499f2a03072
Eirik Meland <eirik.meland@gmail.com>
2021-09-25 19:12:59 +0000

    add test

---
+++ test/Spec.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import Core
+import Core (Step (name))
+import RIO
+
+makeStep :: Text -> Text -> [Text] -> Step
+makeStep name image commands =
+    Step
+        { name = StepName name
+        , image = Image image
+        , commands = commands
+        }
+
+makePipeline :: [Step] -> Pipeline
+makePipeline steps =
+    Pipeline{steps = steps}
+
+-- Test values
+testPipeline :: Pipeline
+testPipeline =
+    makePipeline
+        [ makeStep "First step" "ubuntu" ["date"]
+        , makeStep "Second step" "ubuntu" ["uname -r"]
+        ]
+
+testBuild :: Build
+testBuild =
+    Build
+        { pipeline = testPipeline
+        , state = BuildReady
+        }
+
+main :: IO ()
+main = pure ()
